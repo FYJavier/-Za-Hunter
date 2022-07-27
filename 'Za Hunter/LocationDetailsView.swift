bdb3c31e93117c12bfc74374f21dac00f4ad999b
Freya Javier <thesleepyunicorn63@gmail.com>
2022-07-27 14:19:30 -0500

    Added pizza icons and detail view"\

---
+++ 'Za Hunter/LocationDetailsView.swift
@@ -0,0 +1,25 @@
+//
+//  LocationDetailsView.swift
+//  'Za Hunter
+//
+//  Created by Yuna on 7/27/22.
+//
+
+import SwiftUI
+import MapKit
+
+struct LocationDetailsView: View {
+    var selectedMapItem: MKMapItem
+    var body: some View {
+        VStack {
+            Text(selectedMapItem.name!)
+                .font(.title)
+        }
+    }
+}
+
+struct LocationDetailsView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationDetailsView(selectedMapItem: MKMapItem())
+    }
+}
